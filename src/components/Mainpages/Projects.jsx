07f0a918c44809eab333ade37b31e52182a95d0a
Aditya <as8693164@gmail.com>
2025-11-17 20:47:06 +0530

    up

--- src/components/Mainpages/Projects.jsx
+++ src/components/Mainpages/Projects.jsx
@@ -1,10 +1,51 @@
 import React from 'react'
 import Navbar from '/src/components/navbar/Navbar'
+import './projects.css'
 const Projects = () => {
+  const user_projects =[{
+    project_name :"esper",
+    project_image:"src/assets/projects/esper.png",
+    project_description:"Social media",
+  },
+  {
+    project_name :"Blood unity",
+    project_image:"src/assets/projects/Dronathon.png",
+    project_description:"Blood donation app",
+  },
+  {
+    project_name :"Dronathon",
+    project_image:"src/assets/projects/Dronathon.png",
+    project_description:"Tech-fest side",
+  },
+  {
+    project_name :"more to come soon",
+    project_image:"",
+    project_description:"good things takes time",
+  },
+]
+
+
+
   return (
     <div>
-  <Navbar />
-    </div>
+     
+      <div className='projects-container'>
+        <div className='project-top'>
+          <Navbar />
+        </div>
+        <div className='project-mid'>
+           {
+        user_projects.map((data)=>(
+          <div key={data.id}> 
+           <h3>{data.project_name}</h3>
+            <img src={data.project_image}></img>
+          <p>{data.project_description}</p>
+          </div>
+     )) }
+        </div>
+        </div>
+      </div>
+
   )
 }
 
--- src/components/Mainpages/Skills.jsx
+++ src/components/Mainpages/Skills.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./Skills.css"
 import Navbar from '/src/components/navbar/Navbar'
 import { FaReact, FaNodeJs, FaAws, FaDocker, FaGitAlt, FaJava, FaAndroid } from 'react-icons/fa';
-import {SiMysql, SiSpringboot,SiNetlify, SiHtml5, SiCss3, SiImessage, SiCanva } from 'react-icons/si';
+import { SiMysql, SiSpringboot, SiNetlify, SiHtml5, SiCss3, SiImessage, SiCanva, SiNodedotjs, SiMongodb } from 'react-icons/si';
 const Skills = () => {
   return (
     <div>
@@ -11,18 +11,33 @@ const Skills = () => {
           <Navbar />
         </div>
         <div className='Skills-bottom'>
-           <div>
-            <button>SPRING BOOT<SiSpringboot /></button>
-            <button>REACT JS<FaReact /></button>
-            <button>MYSQL <SiMysql /></button>
-            <button>JAVA <FaJava /></button>
-            <button>Github <FaGitAlt /></button>
-            <button>Netlify <SiNetlify /></button>
-            <button>HTML5 <SiHtml5 /></button>
-             <button>CSS <SiCss3 /></button>
+          <div className='mid'>
+            <div className='mid-skills'>
+              <h2>BACKEND</h2>
+              <button>JAVA <FaJava /></button>
+              <button>SPRING BOOT<SiSpringboot /></button>
+                <button>NODE JS<SiNodedotjs /></button>
+            </div>
+            <div className='mid-skills'>
+              <h2>FRONT-END</h2>
+              <button>HTML5 <SiHtml5 /></button>
+              <button>CSS <SiCss3 /></button>
+              <button>REACT JS<FaReact /></button>
+            </div>
+             
+            <div className='mid-skills'>
+              <h2>DATABASES AND HOSTING</h2>
+              <button>MYSQL <SiMysql /></button>
+              <button>Netlify <SiNetlify /></button>
+              <button>MONGODB <SiMongodb/></button>
+            </div>
+           <div className='mid-skills'>
+                <h2>OTHERS TOOLS</h2>
+                <button>Github <FaGitAlt /></button>
               <button>canva <SiCanva /></button>
               <button>Android <FaAndroid /></button>
-           </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
