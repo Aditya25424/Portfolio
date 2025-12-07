12a2e0bd93b58bea6535e10c51720a5f78d091b4
Aditya sharma <125076850+Aditya25424@users.noreply.github.com>
2025-12-07 23:04:41 +0530

    Refactor routing in App component
    Removed Router component and updated route paths to lowercase.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
+
 import { Homepage } from './components/Homepage.jsx'
 import Welcome from './components/Welcomescreens/Welcome.jsx'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profiles from './components/Welcomescreens/Profiles.jsx';
 import Skills from './components/Mainpages/Skills.jsx';
 import Projects from './components/Mainpages/Projects.jsx';
 import HireMe from './components/Mainpages/HireMe.jsx';
 import Professional from './components/Mainpages/Professional.jsx';
+
 const App = () => {
   return (
-     <Router>
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/homepage" element={<Homepage />} />
-        <Route path="/Profiles" element={<Profiles />} />
-        <Route path="/Skills" element={<Skills />} />
-        <Route path="/Professional" element={<Professional />} />
-        <Route path="/Projects" element={<Projects />} />
-        <Route path="/HireMe" element={<HireMe />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Welcome />} />
+      <Route path="/homepage" element={<Homepage />} />
+      <Route path="/profiles" element={<Profiles />} />
+      <Route path="/skills" element={<Skills />} />
+      <Route path="/professional" element={<Professional />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/hireme" element={<HireMe />} />
+    </Routes>
   )
 }
 
-export default App
+export default App;
